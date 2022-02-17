b9c363f08e8824053b501578eab77fba214d97c7
OzzMO94 <adinovaaa@yandex.ru>
2022-02-17 17:29:29 +0300

    Some editing

---
+++ TrackList/TrackDetailsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TrackDetailsViewController.swift
+//  TrackList
+//
+//  Created by Максим Хабиров on 17.02.2022.
+//
+
+import UIKit
+
+class TrackDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var imageCover: UIImageView!
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    
+    var track: Track!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageCover.image = UIImage(named: track.title)
+        trackTitleLabel.text = track.title
+
+    }
+
+}
--- TrackList/TrackListViewController.swift
+++ TrackList/TrackListViewController.swift
@@ -14,7 +14,7 @@ class TrackListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 80
-
+        navigationItem.rightBarButtonItem = editButtonItem
     }
 
     // MARK: - Table view data source
@@ -38,15 +38,37 @@ class TrackListViewController: UITableViewController {
         
         return cell
     }
-}
+
 
 // MARK: - Table view delegate
 //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 //        80
 //    }
+    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let currentTrack = trackList.remove(at: sourceIndexPath.row)
+        trackList.insert(currentTrack, at: destinationIndexPath.row)
+    }
 
 
 // MARK: - Navigation
-func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let trackDetailsVC = segue.destination as? TrackDetailsViewController else {return}
+        guard let indexPath = tableView.indexPathForSelectedRow else {return}
+        let track = trackList[indexPath.row]
+        trackDetailsVC.track = track
+    }
 }
